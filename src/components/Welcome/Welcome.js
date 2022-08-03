6060923bdbcde2576be30151f37103fecabfbb77
Jarosaw Wabich <wabior@gmail.com>
2022-08-03 13:53:50 +0200

    added deployment conf

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 function Welcome({handleQuizStatus}) {
     return (
-        <div className='mt-3 fs-5 container d-flex flex-column align-items-center justify-content-evenly h-75'>
+        <div className='mt-3 fs-5 container d-flex flex-column align-items-center justify-content-evenly h-75 pb-5'>
             <main>
                 <p>Quiz dotyczy znajomości książki "Siedem nawyków skutecznego działania".</p>
                 <p>Sprawdź, czy jesteś gotowy, by zmienić swoje życie!</p>
             </main>
             <button type={"submit"}
-                    className={'btn btn-outline-primary p-5 fs-5 mt-5'}
+                    className={'btn btn-dark px-5 py-4 fs-5 mt-5'}
                     onClick={ () => handleQuizStatus('started') }
             >Rozpocznij Quiz</button>
         </div>
